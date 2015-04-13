d7e38bb9c49fe5ab6f04e55e1e9ca46c040ad798
Kenneth Henderick <kenneth@ketronic.be>
2015-04-13 14:57:18 +0200

    Remove debug flag

--- source/run.py
+++ source/run.py
@@ -11,6 +11,5 @@ if __name__ == '__main__':
     config = Configuration()
     app.run(host='0.0.0.0',
             port=8500,
-            debug=True,
             ssl_context=context,
             threaded=True)
