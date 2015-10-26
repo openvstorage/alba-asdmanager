066163b7f2285e976ae3bbb2fb35ca9b812609d1
Maxime Daniel <root@maxux.net>
2015-10-26 10:30:31 +0100

    add __sync_dont_use options to master config.json
    - take care about option '__sync_dont_use' in
      /opt/alba-asdmanager/config/config.json (section main)

--- source/app/api.py
+++ source/app/api.py
@@ -154,6 +154,11 @@ class API(object):
                           'asd_id': asd_id,
                           'log_level': 'info',
                           'port': port}
+            
+            if config.data['main'].has_key('__sync_dont_use'):
+                asd_config['__sync_dont_use'] = config.data['main']['__sync_dont_use']
+            
+            
             if ips is not None and len(ips) > 0:
                 asd_config['ips'] = ips
             with open('/mnt/alba-asd/{0}/asd.json'.format(asd_id), 'w') as conffile:
