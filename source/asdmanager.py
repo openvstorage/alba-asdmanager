b85838e2f001b10e6c438532fe2dd2ce520cd4d8
kvanhijf <kevin.vanhijfte@openvstorage.com>
2016-01-18 09:36:43 +0100

    OVS-3942 - Oopsie daisy

--- source/asdmanager.py
+++ source/asdmanager.py
@@ -100,6 +100,6 @@ if __name__ == '__main__':
 
     from source.app import app
     app.run(host='0.0.0.0',
-            port=sys.argv[2],
+            port=sys.argv[1],
             ssl_context=('server.crt', 'server.key'),
             threaded=True)
