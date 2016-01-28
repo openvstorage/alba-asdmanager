abc69186e76f776bd5b39244eb97567c6381319b
pploegaert <philippe.ploegaert@openvstorage.com>
2016-01-28 15:13:43 +0100

    OVS-4080 - add automatic setup for asd-manager

--- source/asdmanager.py
+++ source/asdmanager.py
@@ -54,22 +54,15 @@ def setup():
     if os.path.exists(preconfig):
         config = RawConfigParser()
         config.read(preconfig)
-        api_ip = config.get('asdmanager', 'api_ips')
-        api_port = config.get('asdmanager', 'api_port')
-        asd_start_port = config.get('asdmanager', 'asd_start_port')
+        api_ip = config.get('asdmanager', 'api_ip')
+        api_port = int(config.get('asdmanager', 'api_port'))
+        asd_ips = config.get('asdmanager', 'asd_ips')
+        asd_start_port = int(config.get('asdmanager', 'asd_start_port'))
 
-        if type(api_ip) == list:
-            asd_ips = json.loads(api_ip)
-        else:
-            asd_ips = list()
-            asd_ips.append(api_ip)
-
-        assert type(api_port == int), "api_port should be an integer"
-        assert type(asd_start_port == int), "asd_start_port should be an integer"
-
-        api_port = int(api_port)
-        asd_start_port = int(asd_start_port)
-
+        assert 1024 <= api_port <= 65535, "api_port should be >= 1024 and <= 65535"
+        assert 1024 <= asd_start_port <= 65535, "asd_start_port should be >= 1024 and <= 65535"
+        assert api_port not in range(asd_start_port, asd_start_port + 100), "api_port should not be in asd_port range:"\
+                                                                            "{0} - {} + 100".format(asd_start_port)
     else:
         api_ip = Interactive.ask_choice(ipaddresses, 'Select the public IP address to be used for the API')
         api_port = Interactive.ask_integer("Select the port to be used for the API", 1024, 65535, 8500)
