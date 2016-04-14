f9ba71a38b2f9cd839339ef3dab809dba8140727
Kenneth Henderick <kenneth@ketronic.be>
2016-04-14 13:31:20 +0200

    #70 - Cleaned up logging + More checks and safety

--- source/app/api.py
+++ source/app/api.py
@@ -51,6 +51,10 @@ class API(object):
     CONFIG_ROOT = '/ovs/alba/asdnodes/{0}/config'.format(NODE_ID)
 
     @staticmethod
+    def _log(message):
+        print '{0} - {1}'.format(str(datetime.datetime.now()), message)
+
+    @staticmethod
     @get('/')
     def index():
         """ Return available API calls """
@@ -62,7 +66,7 @@ class API(object):
     @get('/net', authenticate=False)
     def net():
         """ Retrieve IP information """
-        print '{0} - Loading network information'.format(datetime.datetime.now())
+        API._log('Loading network information')
         output = check_output("ip a | grep 'inet ' | sed 's/\s\s*/ /g' | cut -d ' ' -f 3 | cut -d '/' -f 1", shell=True)
         my_ips = output.split('\n')
         return {'ips': [found_ip.strip() for found_ip in my_ips if found_ip.strip() != '127.0.0.1' and found_ip.strip() != ''],
@@ -73,7 +77,7 @@ class API(object):
     @post('/net')
     def set_net():
         """ Set IP information """
-        print '{0} - Setting network information'.format(datetime.datetime.now())
+        API._log('Setting network information')
         EtcdConfiguration.set('{0}/network|ips'.format(API.CONFIG_ROOT), json.loads(request.form['ips']))
         return {'_link': '/net'}
 
@@ -83,31 +87,31 @@ class API(object):
         links = []
         if disk['available'] is False:
             links.append('/disks/{0}/asds'.format(disk_id))
-            actions = ['/disks/{0}/delete'.format(disk_id)]
+            actions = ['/disks/{0}/delete'.format(disk_id),
+                       '/disks/{0}/asds'.format(disk_id)]
             if disk['state']['state'] == 'error':
                 actions.append('/disks/{0}/restart'.format(disk_id))
         else:
-            actions = ['/disks/{0}/add'.format(disk_id),
-                       '/disks/{0}/asds'.format(disk_id)]
+            actions = ['/disks/{0}/add'.format(disk_id)]
         disk['_links'] = links
         disk['_actions'] = actions
 
     @staticmethod
     def _list_disks():
-        print '{0} - Fetching disks'.format(datetime.datetime.now())
+        API._log('Fetching disks')
         disks = Disks.list_disks()
         for disk_id in disks:
             disks[disk_id]['name'] = disk_id
             disks[disk_id]['node_id'] = API.NODE_ID
             API._disk_hateoas(disks[disk_id], disk_id)
-        print '{0} - Fetching disks completed'.format(datetime.datetime.now())
+        API._log('Fetching disks completed')
         return disks
 
     @staticmethod
     @get('/disks')
     def list_disks():
         """ List all disk information """
-        print '{0} - Listing disks'.format(datetime.datetime.now())
+        API._log('Listing disks')
         data = API._list_disks()
         data['_parent'] = '/'
         data['_actions'] = []
@@ -120,7 +124,7 @@ class API(object):
         Retrieve information about a single disk
         :param disk: Identifier of the disk
         """
-        print '{0} - Listing disk {1}'.format(datetime.datetime.now(), disk)
+        API._log('Listing disk {0}'.format(disk))
         all_disks = API._list_disks()
         if disk not in all_disks:
             raise BadRequest('Disk unknown')
@@ -136,9 +140,9 @@ class API(object):
         Add a disk
         :param disk: Identifier of the disk
         """
-        print '{0} - Add disk {1}'.format(datetime.datetime.now(), disk)
+        API._log('Add disk {0}'.format(disk))
         with FileMutex('add_disk'), FileMutex('disk_'.format(disk)):
-            print '{0} - Got lock for add disk {1}'.format(datetime.datetime.now(), disk)
+            API._log('Got lock for add disk {0}'.format(disk))
             all_disks = API._list_disks()
             if disk not in all_disks:
                 raise BadRequest('Disk not available')
@@ -146,10 +150,10 @@ class API(object):
                 raise BadRequest('Disk already configured')
 
             # Partitioning and mounting
-            print '{0} - Preparing disk {1}'.format(datetime.datetime.now(), disk)
+            API._log('Preparing disk {0}'.format(disk))
             Disks.prepare_disk(disk)
 
-            print '{0} - Returning info about added disk {1}'.format(datetime.datetime.now(), disk)
+            API._log('Returning info about added disk {0}'.format(disk))
             all_disks = API._list_disks()
             data = all_disks[disk]
             API._disk_hateoas(data, disk)
@@ -163,17 +167,23 @@ class API(object):
         Delete a disk
         :param disk: Identifier of the disk
         """
-        print '{0} - Deleting disk {1}'.format(datetime.datetime.now(), disk)
+        API._log('Deleting disk {0}'.format(disk))
         with FileMutex('disk_'.format(disk)):
-            print '{0} - Got lock for deleting disk {1}'.format(datetime.datetime.now(), disk)
+            API._log('Got lock for deleting disk {0}'.format(disk))
             all_disks = API._list_disks()
             if disk not in all_disks:
                 raise BadRequest('Disk not available')
             if all_disks[disk]['available'] is True:
                 raise BadRequest('Disk not yet configured')
+            mountpoints = FSTab.read()
+            if disk in mountpoints:
+                mountpoint = mountpoints[disk]
+                asds = API._list_asds_disk(disk, mountpoint)
+                if len(asds) != 0:
+                    raise BadRequest('There are still ASDs configured on disk {0}'.format(disk))
 
             # Cleanup & unmount disk
-            print '{0} - Cleaning disk {1}'.format(datetime.datetime.now(), disk)
+            API._log('Cleaning disk {0}'.format(disk))
             Disks.clean_disk(disk)
 
             return {'_link': '/disks/{0}'.format(disk)}
@@ -185,14 +195,20 @@ class API(object):
         Restart a disk
         :param disk: Identifier of the disk
         """
-        print '{0} - Restarting disk {1}'.format(datetime.datetime.now(), disk)
+        API._log('Restarting disk {0}'.format(disk))
         with FileMutex('disk_'.format(disk)):
-            print '{0} - Got lock for restarting disk {1}'.format(datetime.datetime.now(), disk)
+            API._log('Got lock for restarting disk {0}'.format(disk))
             all_disks = API._list_disks()
             if disk not in all_disks:
                 raise BadRequest('Disk not available')
             if all_disks[disk]['available'] is True:
                 raise BadRequest('Disk not yet configured')
+            mountpoints = FSTab.read()
+            if disk in mountpoints:
+                mountpoint = mountpoints[disk]
+                asds = API._list_asds_disk(disk, mountpoint)
+                if len(asds) != 0:
+                    raise BadRequest('There are still ASDs configured on disk {0}'.format(disk))
 
             # Remount the disk
             Disks.remount_disk(disk)
@@ -247,7 +263,7 @@ class API(object):
         """
         mountpoints = FSTab.read()
         if disk not in mountpoints:
-            raise RuntimeError('Disk {0} is not yet initialized'.format(disk))
+            raise BadRequest('Disk {0} is not yet initialized'.format(disk))
         mountpoint = mountpoints[disk]
         asds = API._list_asds_disk(disk, mountpoint)
         asds['_parent'] = '/disks/{0}'.format(disk)
@@ -263,44 +279,45 @@ class API(object):
         """
         mountpoints = FSTab.read()
         if disk not in mountpoints:
-            raise RuntimeError('Disk {0} is not yet initialized'.format(disk))
-        asd_id = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32))
-        all_asds = {}
-        mountpoints = FSTab.read()
-        for disk, mountpoint in mountpoints.iteritems():
-            all_asds.update(API._list_asds_disk(disk, mountpoint))
-        mountpoint = mountpoints[disk]
+            raise BadRequest('Disk {0} is not yet initialized'.format(disk))
+        with FileMutex('add_asd'):
+            asd_id = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32))
+            all_asds = {}
+            mountpoints = FSTab.read()
+            for disk, mountpoint in mountpoints.iteritems():
+                all_asds.update(API._list_asds_disk(disk, mountpoint))
+            mountpoint = mountpoints[disk]
 
-        # Prepare & start service
-        print '{0} - Setting up service for disk {1}'.format(datetime.datetime.now(), disk)
-        homedir = '{0}/{1}'.format(mountpoint, asd_id)
-        port = EtcdConfiguration.get('{0}/network|port'.format(API.CONFIG_ROOT))
-        ips = EtcdConfiguration.get('{0}/network|ips'.format(API.CONFIG_ROOT))
-        used_ports = [all_asds[asd]['port'] for asd in all_asds]
-        while port in used_ports:
-            port += 1
-        asd_config = {'home': homedir,
-                      'node_id': API.NODE_ID,
-                      'asd_id': asd_id,
-                      'log_level': 'info',
-                      'port': port}
+            # Prepare & start service
+            API._log('Setting up service for disk {0}'.format(disk))
+            homedir = '{0}/{1}'.format(mountpoint, asd_id)
+            port = EtcdConfiguration.get('{0}/network|port'.format(API.CONFIG_ROOT))
+            ips = EtcdConfiguration.get('{0}/network|ips'.format(API.CONFIG_ROOT))
+            used_ports = [all_asds[asd]['port'] for asd in all_asds]
+            while port in used_ports:
+                port += 1
+            asd_config = {'home': homedir,
+                          'node_id': API.NODE_ID,
+                          'asd_id': asd_id,
+                          'log_level': 'info',
+                          'port': port}
 
-        if EtcdConfiguration.exists('{0}/extra'.format(API.CONFIG_ROOT)):
-            data = EtcdConfiguration.get('{0}/extra'.format(API.CONFIG_ROOT))
-            for extrakey in data:
-                asd_config[extrakey] = data[extrakey]
+            if EtcdConfiguration.exists('{0}/extra'.format(API.CONFIG_ROOT)):
+                data = EtcdConfiguration.get('{0}/extra'.format(API.CONFIG_ROOT))
+                for extrakey in data:
+                    asd_config[extrakey] = data[extrakey]
 
-        if ips is not None and len(ips) > 0:
-            asd_config['ips'] = ips
-        EtcdConfiguration.set(API.ASD_CONFIG.format(asd_id), json.dumps(asd_config), raw=True)
+            if ips is not None and len(ips) > 0:
+                asd_config['ips'] = ips
+            EtcdConfiguration.set(API.ASD_CONFIG.format(asd_id), json.dumps(asd_config), raw=True)
 
-        service_name = '{0}{1}'.format(API.ASD_SERVICE_PREFIX, asd_id)
-        params = {'ASD': asd_id,
-                  'SERVICE_NAME': service_name}
-        os.mkdir(homedir)
-        check_output('chown -R alba:alba {0}'.format(homedir), shell=True)
-        ServiceManager.add_service('alba-asd', local_client, params, service_name)
-        ServiceManager.start_service(service_name, local_client)
+            service_name = '{0}{1}'.format(API.ASD_SERVICE_PREFIX, asd_id)
+            params = {'ASD': asd_id,
+                      'SERVICE_NAME': service_name}
+            os.mkdir(homedir)
+            check_output('chown -R alba:alba {0}'.format(homedir), shell=True)
+            ServiceManager.add_service('alba-asd', local_client, params, service_name)
+            ServiceManager.start_service(service_name, local_client)
 
     @staticmethod
     @post('/disks/<disk>/asds/<asd_id>/delete')
@@ -311,16 +328,16 @@ class API(object):
         :param asd_id: The ASD ID of the ASD to be removed
         """
         # Stop and remove service
-        print '{0} - Removing services for disk {1}'.format(datetime.datetime.now(), disk)
+        API._log('Removing services for disk {0}'.format(disk))
         mountpoints = FSTab.read()
         if disk not in mountpoints:
-            raise RuntimeError('Disk {0} is not yet initialized'.format(disk))
+            raise BadRequest('Disk {0} is not yet initialized'.format(disk))
         all_asds = {}
         mountpoints = FSTab.read()
         for disk, mountpoint in mountpoints.iteritems():
             all_asds.update(API._list_asds_disk(disk, mountpoint))
         if asd_id not in all_asds:
-            raise RuntimeError('Could not find ASD {0} on disk {1}'.format(asd_id, disk))
+            raise BadRequest('Could not find ASD {0} on disk {1}'.format(asd_id, disk))
         mountpoint = mountpoints[disk]
         service_name = '{0}{1}'.format(API.ASD_SERVICE_PREFIX, asd_id)
         if ServiceManager.has_service(service_name, local_client):
@@ -351,8 +368,8 @@ class API(object):
     @staticmethod
     def _get_package_information(package_name):
         installed, candidate = PackageManager.get_installed_candidate_version(package_name, local_client)
-        print '{0} - Installed version for package {1}: {2}'.format(datetime.datetime.now(), package_name, installed)
-        print '{0} - Candidate version for package {1}: {2}'.format(datetime.datetime.now(), package_name, candidate)
+        API._log('Installed version for package {0}: {1}'.format(package_name, installed))
+        API._log('Candidate version for package {0}: {1}'.format(package_name, candidate))
         return installed, candidate
 
     @staticmethod
@@ -376,7 +393,7 @@ class API(object):
     def get_update_information():
         """ Retrieve update information """
         with FileMutex('package_update'):
-            print '{0} - Locking in place for package update'.format(datetime.datetime.now())
+            API._log('Locking in place for package update')
             API._update_packages()
             sdm_package_info = API._get_package_information(package_name=API.PACKAGE_NAME)
             sdm_installed = sdm_package_info[0]
@@ -406,7 +423,7 @@ class API(object):
 
             if sdm_package_info[0] != sdm_package_info[1]:
                 if status == 'started':
-                    print '{0} - Updating package {1}'.format(datetime.datetime.now(), API.PACKAGE_NAME)
+                    API._log('Updating package {0}'.format(API.PACKAGE_NAME))
                     check_output('echo "python {0} >> /var/log/upgrade-openvstorage-sdm.log 2>&1" > /tmp/update'.format(API.INSTALL_SCRIPT), shell=True)
                     check_output('at -f /tmp/update now', shell=True)
                     check_output('rm /tmp/update', shell=True)
@@ -427,18 +444,16 @@ class API(object):
                 if running_version != alba_package_info[1]:
                     status = ServiceManager.get_service_status(service, local_client)
                     if status is False:
-                        print "{0} - Found stopped service {1}. Will not start it.".format(datetime.datetime.now(),
-                                                                                           service)
+                        API._log('Found stopped service {0}. Will not start it.'.format(service))
                         result[service] = 'stopped'
                     else:
-                        print '{0} - Restarting service {1}'.format(datetime.datetime.now(), service)
+                        API._log('Restarting service {0}'.format(service))
                         try:
                             status = ServiceManager.restart_service(service, local_client)
-                            print '{0} - {1}'.format(datetime.datetime.now(), status)
+                            API._log(status)
                             result[service] = 'restarted'
                         except CalledProcessError as cpe:
-                            print "{0} - Failed to restart service {1} {2}".format(datetime.datetime.now(), service,
-                                                                                   cpe)
+                            API._log('Failed to restart service {0} {1}'.format(service, cpe))
                             result[service] = 'failed'
 
             return {'result': result}
@@ -462,7 +477,7 @@ class API(object):
     @get('/maintenance')
     def list_maintenance_services():
         """ List all maintenance information """
-        print '{0} - Listing maintenance services'.format(datetime.datetime.now())
+        API._log('Listing maintenance services')
         data = API._list_maintenance_services()
         data['_parent'] = '/'
         data['_actions'] = []
