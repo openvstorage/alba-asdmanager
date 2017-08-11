a5bdd2bec2228947b6bb52648e37d6da09be9831
Jeffrey Devloo <jeffreydevloo@hotmail.com>
2017-08-11 16:03:24 +0200

    Fix error on first request

--- source/asdmanager.py
+++ source/asdmanager.py
@@ -326,7 +326,7 @@ if __name__ == '__main__':
         if app.debug is False:
             _logger = LogHandler.get('asd-manager', name='flask')
             app.logger.handlers = []
-            app.logger.addHandler(_logger.handler)
+            app.logger.addHandler(_logger.logger.handlers[0])
             app.logger.propagate = False
             wz_logger = logging.getLogger('werkzeug')
             wz_logger.handlers = []
