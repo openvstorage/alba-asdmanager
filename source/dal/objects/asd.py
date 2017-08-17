1c1ce3a953aed9dc4993293e2fa21a741101c8b5
Kevin Van Hijfte <kevin.vanhijfte@openvstorage.com>
2017-08-17 11:12:21 +0200

    Fixing unittests

--- source/dal/objects/asd.py
+++ source/dal/objects/asd.py
@@ -63,7 +63,10 @@ class ASD(ASDBase):
             raise RuntimeError('No configuration found for ASD {0}'.format(self.asd_id))
         data = Configuration.get(self.config_key)
         for prop in self._properties:
-            data[prop.name] = getattr(self, prop.name)
+            if prop.name == 'hosts':
+                data['ips'] = getattr(self, prop.name)
+            else:
+                data[prop.name] = getattr(self, prop.name)
         if self.disk.state == 'MISSING':
             data.update({'state': 'error',
                          'state_detail': 'missing'})
