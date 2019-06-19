6e0125e90e90b8b4b64da33a84d3a8e77a4b19d0
Simon Van den Bossche <simon.vandenbossche@openvstorage.com>
2019-06-19 15:55:31 +0200

    Streamline offloading

--- source/tools/update/albacomponent_update.py
+++ source/tools/update/albacomponent_update.py
@@ -1,10 +1,5 @@
-from abc import abstractmethod
-from ovs_extensions.update.albacomponent_update import AlbaComponentUpdater as _albacomponent_updater
-from ovs_extensions.storage.persistent.persistentfactory import PersistentFactory
+from ovs_extensions.update.alba_component_update import AlbaComponentUpdater as _albacomponent_updater
 from source.tools.system import System
-from source.tools.servicefactory import ServiceFactory
-
-logger = logging.getLogger(__name__)
 
 
 class AlbaComponentUpdater(_albacomponent_updater):
@@ -12,22 +7,11 @@ class AlbaComponentUpdater(_albacomponent_updater):
     Implementation of abstract class to update alba
     """
 
-    SERVICE_MANAGER = ServiceFactory.get_manager()
-
-    @staticmethod
-    @abstractmethod
-    def get_persistent_client():
-        # type: () -> PyrakoonStore
-        """
-        Retrieve a persistent client which needs
-        Needs to be implemented by the callee
-        """
-        return PersistentFactory.get_client()
-
     @classmethod
     def get_node_id(cls):
+        # type: () -> str
+        """
+        Use a factory to return the node id
+        :return:
+        """
         return System.get_my_machine_id()
-
-
-if __name__ == '__main__':
-    print AlbaComponentUpdater.restart_services()
