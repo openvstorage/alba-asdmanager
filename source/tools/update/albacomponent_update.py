d6a88905eea12e9c7dd34d822ac6542ac6897810
Simon Van den Bossche <simon.vandenbossche@openvstorage.com>
2019-06-21 17:01:14 +0200

    Fixed remarks, pretified code

--- source/tools/update/albacomponent_update.py
+++ source/tools/update/albacomponent_update.py
@@ -1,8 +1,25 @@
-from ovs_extensions.update.alba_component_update import AlbaComponentUpdater as _albacomponent_updater
+# Copyright (C) 2019 iNuron NV
+#
+# This file is part of Open vStorage Open Source Edition (OSE),
+# as available from
+#
+#      http://www.openvstorage.org and
+#      http://www.openvstorage.com.
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License v3 (GNU AGPLv3)
+# as published by the Free Software Foundation, in version 3 as it comes
+# in the LICENSE.txt file of the Open vStorage OSE distribution.
+#
+# Open vStorage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY of any kind.
+
+
+from ovs_extensions.update.alba_component_update import AlbaComponentUpdater as _AlbacomponentUpdater
 from source.tools.system import System
 
 
-class AlbaComponentUpdater(_albacomponent_updater):
+class AlbaComponentUpdater(_AlbacomponentUpdater):
     """
     Implementation of abstract class to update alba
     """
